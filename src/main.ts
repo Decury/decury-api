0f93c473b81e3e53042d09f413c11a30a52d480d
BuildTools <leungyauming2005@gmail.com>
2020-03-16 22:01:02 +0800

    Implement parsing for cli port flag

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import v1 from "./v1";
 
-const decury = express();
 
+import { program } from "commander";
+
+program.option("-p, --port <number>", "api port", parseInt, 8080);
+program.parse(process.argv);
+
+
+const decury = express();
 decury.use("/v1", v1);
 decury.use((req, res) => res.status(404).send({"status": 404}));
 
-decury.listen(8080);
+if (
+    typeof program.port == "number" &&
+    program.port <= 65535 &&
+    program.port >= 1
+) decury.listen(program.port);
